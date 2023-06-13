d84905a40db7ee032b5550cefbb04219261f0a6a
ShadowOfBeing <82723137+ShadowOfBeing@users.noreply.github.com>
2023-06-13 19:01:26 +0300

    update 19:01 13.06.2023

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+answersAndQuestions = {
+    'work_place': {
+        0: {'question': 'work_place question 1', 'answer': 'work_place answer 1'},
+        1: {'question': 'work_place question 2', 'answer': 'work_place answer 2'},
+        2: {'question': 'work_place question 3', 'answer': 'work_place answer 3'},
+    },
+    'python': {
+        0: {'question': 'python question 1', 'answer': 'python answer 1'},
+        1: {'question': 'python question 2', 'answer': 'python answer 2'},
+        2: {'question': 'python question 3', 'answer': 'python answer 3'},
+    },
+    'databases': '',
+    'big_o_notations': '',
+    'oop': '',
+    'django': '',
+    'git': '',
+    'docker': '',
+    'drf': '',
+    'brokers': '',
+}
+
+function changePage(category) {
+//localStorage.setItem("myKey", "myValue");
+//    if (category == 'main') {
+//        var resultHTML = `<div class="content-box">Главная страница</div>`
+//    } else {
+//        var questionsMenu = document.getElementsByClassName('questions-menu')[0]
+//        var qa = answersAndQuestions[category]
+//        var questionsMenuHTML = ''
+//        for (var i = 0; i < Object.keys(qa).length; i++) {
+//            questionsMenuHTML += `<div id="${i + 1}" class="question-in-menu" onclick="showAnswer(${i + 1})">${qa[i]['question']}</div>`
+//        }
+//        questionsMenu.innerHTML = questionsMenuHTML
+//    }
+}
+
+function getQuestion() {
+    alert(1);
+}
+
+function showAnswer(option) {
+    alert(1);
+}
